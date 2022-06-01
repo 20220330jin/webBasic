b3bc856069dfee082c471ec10e3fbff1b27e588a
jin <20220330jin@gmail.com>
2022-06-01 17:15:16 +0900

    [30Days] JS variables
    variables: String, Number, Array, Object

---
+++ main/first_js/app.js
@@ -0,0 +1,23 @@
+let greetingText = 'Hi I\'m Jin.';
+let age = 32;
+// alert(greetingText)
+greetingText = 'what?'
+// alert(greetingText)
+// alert(age)
+
+let userName = 'Jin';
+let hobby1 = 'Study';
+let hobby2 = 'Reading';
+
+let hobbies = [hobby1, hobby2, age];
+
+console.log(hobbies)
+console.log(hobbies[2])
+
+let job = {
+    title: 'developer',
+    place: 'Seoul',
+}
+
+console.log(job);
+console.log(job.title);
