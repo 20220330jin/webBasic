53a289eace7c24838501425cc4b7ac082bb1befe
jin <20220330jin@gmail.com>
2022-06-04 21:38:05 +0900

    [33Days] JS Drill into DOM

--- main/js_dom/app.js
+++ main/js_dom/app.js
@@ -1,1 +1,11 @@
-console.log(window);
+// console.log(window.document);
+
+console.log(document);
+console.dir(document);
+
+// console.log(document.body.children[1]);
+
+console.dir(document.body)
+
+document.body.children[1].children[0].href = 'https://google.com';
+
