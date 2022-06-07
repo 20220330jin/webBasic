9bc202c30c84ee62248e57ecb7d975a4ad404992
jin <20220330jin@gmail.com>
2022-06-08 08:21:42 +0900

    [34Days] JS Query DOM

--- main/js_dom/app.js
+++ main/js_dom/app.js
@@ -6,6 +6,19 @@ console.dir(document);
 // console.log(document.body.children[1]);
 
 console.dir(document.body)
+console.dir(document.body.children)
 
 document.body.children[1].children[0].href = 'https://google.com';
 
+console.log('query')
+
+let anchorElement = document.getElementById('external-link');
+
+console.log(anchorElement);
+console.log(anchorElement.href);
+
+let anchorElementByQuery = document.querySelector('#external-link');
+
+anchorElementByQuery.href = 'https://www.naver.com';
+
+console.log(anchorElementByQuery);
