2085d23f0f5b278c5800e2bd2e4f3c2cadccc2f1
jin <20220330jin@gmail.com>
2022-06-15 08:16:54 +0900

    [35Days] remove and move elements

--- main/js_dom/app.js
+++ main/js_dom/app.js
@@ -17,11 +17,11 @@ let anchorElement = document.getElementById('external-link');
 console.log(anchorElement);
 console.log(anchorElement.href);
 
-let anchorElementByQuery = document.querySelector('#external-link');
+// let anchorElementByQuery = document.querySelector('#external-link');
 
-anchorElementByQuery.href = 'https://www.naver.com';
+// anchorElementByQuery.href = 'https://www.naver.com';
 
-console.log(anchorElementByQuery);
+// console.log(anchorElementByQuery);
 
 console.log('-------------');
 
@@ -38,3 +38,14 @@ console.log(firstParagraph);
 
 // 3. Intert new element into the parent element content
 firstParagraph.append(newAnchorElement);
+
+// Remove Elements
+// 1. Select the element that should be removed
+let firstH1Element = document.querySelector('h1');
+firstH1Element.remove();
+firstH1Element.parentElement.removeChild(firstH1Element);
+
+// MOVE ELEMENTS
+
+// 1. 
+firstParagraph.parentElement.append(firstParagraph)
