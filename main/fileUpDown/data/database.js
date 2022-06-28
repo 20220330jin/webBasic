eb7f0a819664b07251eba230f41ab8f7774a19c6
jin <20220330jin@gmail.com>
2022-06-29 08:20:44 +0900

    [67Days] file Upload Front code settings

---
+++ main/fileUpDown/data/database.js
@@ -0,0 +1,32 @@
+// const mongodb = require('mongodb');
+
+// const MongoClient = mongodb.MongoClient;
+
+// let database;
+
+// async function connectToDatabase() {
+//   const client = await MongoClient.connect('mongodb://localhost:27017');
+//   database = client.db('file-demo');
+// }
+
+// function getDb() {
+//   if (!database) {
+//     throw { message: 'Database not connected!' };
+//   }
+//   return database;
+// }
+
+// module.exports = {
+//   connectToDatabase: connectToDatabase,
+//   getDb: getDb,
+// };
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    database: 'blog',
+    user: 'root',
+    password: '545103',
+});
+
+module.exports = pool;
