ef79b7f8ad3058065d99e000addd44a366cb0fa0
jin <20220330jin@gmail.com>
2022-06-28 22:12:31 +0900

    [62Days] select query - detail

--- main/blog/routes/blog.js
+++ main/blog/routes/blog.js
@@ -38,6 +38,26 @@ router.get('/new-post', async function (req, res) {
     res.render('create-post', { authors: authors });
 })
 
+router.get('/posts/:id', async function (req, res) {
+    const post_id = req.params.id;
+    const query = `
+        SELECT posts.*, authors.name AS author_name, authors.email AS author_email
+        FROM posts
+        INNER JOIN authors
+        ON posts.author_id = authors.id
+        WHERE posts.id = ?
+    `;
+    // const [post] = await db.query(query);
+    const [posts] = await db.query(query, [post_id]);
+
+    if (!posts || posts.length === 0) {
+        return res.status(404).render('404');
+    }
+
+    res.render('post-detail', { post: posts[0] });
+})
+
+
 
 
 module.exports = router;
